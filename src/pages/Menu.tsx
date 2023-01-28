a3b118819907e34b09bf7449c9e4170f91b6fcf0
bannerett <33360251+bannerett@users.noreply.github.com>
2023-01-28 05:36:01 +0100

    chore: set up page transition

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,7 +1,19 @@
 import { Box } from '@chakra-ui/react';
+import AnimatePage from '../components/AnimatePage';
+import { Helmet } from 'react-helmet-async';
+import { contentStyles } from '../constants/contentStyles';
 
 function Menu() {
-  return <Box>Menu</Box>;
+  return (
+    <AnimatePage>
+      <Helmet>
+        <title>Little Lemon - Menu</title>
+      </Helmet>
+      <Box as="section" {...contentStyles} maxW={900} mx="auto" py={8}>
+        Menu
+      </Box>
+    </AnimatePage>
+  );
 }
 
 export default Menu;
