0b3844ddb5cf679d050b8d269ff14f099f6acfc5
bannerett <33360251+bannerett@users.noreply.github.com>
2023-02-01 02:59:18 +0100

    refactor: basic markup

---
+++ src/components/AboutDescription.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+
+function AboutDescription() {
+  return <Box h={560}>About</Box>;
+}
+
+export default AboutDescription;
---
+++ src/components/Testimonials.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+
+function Testimonials() {
+  return <Box h={590}>Testimonials</Box>;
+}
+
+export default Testimonials;
